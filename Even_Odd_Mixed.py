c5148c9b49b7744f33bde7203c833fba2a9b8f45
20a91a0527 <84711273+20a91a0527@users.noreply.github.com>
2022-09-23 10:52:33 +0530

    a=int(input()) c=d=0 while a>0:     r=a%10     if r%2==0:         c+=1     elif r%2!=0:         d+=1     a=a//10 if c>0 and d>0:     print("Mixed") elif c==0 and d>0:     print("Odd") else:     print("Even")

---
+++ Even_Odd_Mixed.py
@@ -0,0 +1,15 @@
+a=int(input())
+c=d=0
+while a>0:
+    r=a%10
+    if r%2==0:
+        c+=1
+    elif r%2!=0:
+        d+=1
+    a=a//10
+if c>0 and d>0:
+    print("Mixed")
+elif c==0 and d>0:
+    print("Odd")
+else:
+    print("Even")
